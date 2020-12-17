9ba49492e5ae0f0c519668964abbd69a5d3480ef
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-17 23:23:44 +0900

    Add image files into asset catalog , and Create custom scene file

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by 櫻井将太郎 on 2020/12/17.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameScene: SKScene {
+
+	
+
+}
