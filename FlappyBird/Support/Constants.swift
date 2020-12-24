e34e3e4dffb5df2c5fb10c9f43f70a2f086fb883
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-24 23:21:27 +0900

    Re struct sound files in project, Update constants file

--- FlappyBird/Support/Constants.swift
+++ FlappyBird/Support/Constants.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 struct C {
+
+	// MARK: - Image file names
 	static let IMG_ASSET_GROUND: String = "ground"
 	static let IMG_ASSET_CLOUD: String = "cloud"
 	static let IMG_ASSET_WALL: String = "wall"
@@ -16,6 +18,11 @@ struct C {
 	static let IMG_ASSET_BIRD_B: String = "bird_b"
 	static let IMG_ASSET_ITEM: String = "item"
 
+	// MARK: - Key string for user defaults
 	static let BEST_SCORE_KEY: String = "BEST"
 
+	// MARK: - Sound file name
+	static let SOUND_GET_ITEM: String = "get_item.mp3"
+	static let SOUND_GAMEOVER: String = "gameover.mp3"
+	
 }
